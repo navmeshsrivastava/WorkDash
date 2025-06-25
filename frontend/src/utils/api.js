525de0466c4a4ce8964cfdcb31c0aedeb06c82f9
Navmesh Srivastava <navmeshsrivastav815@gmail.com>
2025-06-25 15:29:06 +0530

    Changing URL as free hosting hit its limit

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 const API_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://workdash.onrender.com'
+    ? 'https://workdash-gb23.onrender.com'
     : 'http://localhost:4000';
 
 export { API_URL };
